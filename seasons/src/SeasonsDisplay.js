9957636200afe971e1fcd71b514f2c042e712a8a
Abhinav Saurav <abhinavsaurav1@gmail.com>
2020-08-05 11:52:22 +0530

    Sesons app completed

--- seasons/src/SeasonsDisplay.js
+++ seasons/src/SeasonsDisplay.js
@@ -1,5 +1,38 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
-const SeasonsDisplay = () => {
-	return <div>Seasons display</div>;
+const seasonConfig = {
+	summer: {
+		text: "Let's hit the beach",
+		iconName: "sun",
+	},
+	winter: {
+		text: "Burr, its chilly",
+		iconName: "snowflake",
+	},
 };
+
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? "summer" : "winter";
+	} else {
+		return lat > 0 ? "winter" : "summer";
+	}
+};
+
+const SeasonsDisplay = (props) => {
+	const season = getSeason(props.lat, new Date().getMonth());
+	// console.log(season);
+	// const text =
+	// 	season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+	const { text, iconName } = seasonConfig[season];
+	return (
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left ${iconName} massive loading icon`} />
+			<h1>{text}</h1>
+			<i className={`icon-right ${iconName} massive loading icon`} />
+		</div>
+	);
+};
+
+export default SeasonsDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,24 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonsDisplay from "./SeasonsDisplay";
+import Spinner from "./Spinner";
 // we are attaching only one method attached to  it. Normally,
 // react expects us to attach a lot of classes which is why we
 // are importing React.Component
 
 class App extends React.Component {
-	constructor(props) {
-		// if we don't use super then we get an error
-		super(props);
+	// constructor(props) {
+	// 	// if we don't use super then we get an error
+	// 	super(props);
 
-		/**
-		 * This is the only time we do direct assignment
-		 */
-		this.state = { lat: null, errMessage: "" };
+	// 	/**
+	// 	 * This is the only time we do direct assignment
+	// 	 */
+	// 	this.state = { lat: null, errMessage: "" };
+	// }
 
+	// Alternate intialization directly
+	state = { lat: null, errMessage: "", time: null };
+
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => {
 				// console.log(position);
-				this.setState({ lat: position.coords.latitude });
+				this.setState({
+					lat: position.coords.latitude,
+				});
 
 				//we should never write direct assignment to this.state.lat= position.coords.latitude
 			},
@@ -28,19 +36,24 @@ class App extends React.Component {
 		);
 	}
 
+	renderContent() {
+		if (this.state.errMessage && !this.state.lat) {
+			return <div>error: {this.state.errMessage}</div>;
+		} else if (!this.state.errMessage && this.state.lat) {
+			return <SeasonsDisplay lat={this.state.lat} />;
+		} else {
+			return (
+				<Spinner message="Please allow the location request to continue.." />
+			);
+		}
+	}
+
 	// React says we have to define render !!
 	render() {
 		// Also, we shouldn't call the fucntion inside render because it
 		// will get called again and again for re-rendering so, its gonna make some time if we call it here
-
 		// Also direct object assignment will throw error
-		if (this.state.errMessage && !this.state.lat) {
-			return <div>error: {this.state.errMessage}</div>;
-		} else if (!this.state.errMessage && this.state.lat) {
-			return <div>Lat: {this.state.lat}</div>;
-		} else {
-			return <div>Loading</div>;
-		}
+		return <div className="border red">{this.renderContent()}</div>;
 	}
 }
 
