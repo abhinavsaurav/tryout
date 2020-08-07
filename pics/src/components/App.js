270b854ac49961793461b84cb338a521e16bfd83
Abhinav Saurav <abhinavsaurav1@gmail.com>
2020-08-07 16:01:24 +0530

    API request working

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,32 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-	return (
-		<div className="ui container" style={{ marginTop: "10px" }}>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends React.Component {
+	state = { images: [] };
+
+	onSearchSubmit = async (term) => {
+		// console.log(term);
+
+		const response = await axios.get("https://api.unsplash.com/search/photos", {
+			params: { query: term },
+			headers: {
+				Authorization: "Client-ID OaS7Py8RcrHORU7h_CzwB64BHaSqjcKwuTaB6cJbxP0",
+			},
+		});
+		// console.log(response.data.results[0].urls.raw);
+		this.setState({ images: response.data.results });
+	};
+
+	// the onSubmitBro is being passed as a param
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<SearchBar onSubmitBro={this.onSearchSubmit} />
+				Found: {this.state.images.length} images
+			</div>
+		);
+	}
+}
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,27 +3,32 @@ import React from "react";
 class SearchBar extends React.Component {
 	state = { term: "" };
 
+	// Alternate way of solving the context problem
 	// constructor() {
 	// 	super();
 	// 	this.onFormSubmit = this.onFormSubmit.bind(this);
 	// }
 
-	onInputChange(event) {
-		console.log(event.target.value);
-	}
-
 	onFormSubmit = (event) => {
 		event.preventDefault();
-		console.log(this.state.term);
+
+		/**
+		 * onSubmitBro is a prop in the App.js file and this file is getting included there via webpack and import
+		 *
+		 * Explanation for below -> Since it will get included in one main file so all the code here will be in a file
+		 * and then we are calling the onSubmitBro
+		 * since the onSubmitBro has a function assigned to it the parameter will get passed as function
+		 * and then it will be printed by the console.log
+		 *
+		 */
+		this.props.onSubmitBro(this.state.term);
+		// console.log(this.state.term);
 	};
 
 	render() {
 		return (
 			<div className="ui segment">
-				<form
-					onSubmit={(event) => this.onFormSubmit(event)}
-					className="ui form"
-				>
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Image Search</label>
 						<input
