e78afe10f24bb4a7f2c571cd6cd886fb41843b98
Abhinav Saurav <abhinavsaurav1@gmail.com>
2020-08-10 19:50:53 +0530

    Finished Pics App

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+	constructor(props) {
+		super(props);
+		this.imageRef = React.createRef();
+		this.state = { spans: 0 };
+	}
+
+	componentDidMount() {
+		this.imageRef.current.addEventListener("load", this.setSpans);
+		// console.log(this.imageRef);
+	}
+
+	//remeber for arrow funct otherwise bind error or this error
+	setSpans = () => {
+		const height = this.imageRef.current.clientHeight;
+		const spans = Math.ceil(height / 10);
+		this.setState({ spans }); // or this.setState({spans:spans}); ES6 syntax
+	};
+
 	render() {
 		const { description, urls } = this.props.image;
+		// inline styling in the div
 		return (
-			<div>
-				<img src={urls.regular} alt={description} />
+			<div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+				<img ref={this.imageRef} src={urls.regular} alt={description} />
 			</div>
 		);
 	}
